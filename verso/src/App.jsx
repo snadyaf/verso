10f1a379b08cee04e3f84b693b737bba34797301
Sids-root <sidlopes11@gmail.com>
2026-09-03 22:39:39 -0300

    Finalizado o navbar, ainda falta colocar para funcionar os links

--- verso/src/App.jsx
+++ verso/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   
   return (
     <>
-     <h1>Funcional</h1>
+     <Navbar />
     </>
   )
 }
